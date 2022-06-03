0b5f801abd12526ce8a7d10c24988cae0dd0f0d6
javims95 <javier_ms95@hotmail.com>
2022-06-03 20:40:38 +0200

    Update backend
    Create method GET, POST, PUT and DELETE

--- backend/class/shipping_address.class.php
+++ backend/class/shipping_address.class.php
@@ -4,12 +4,201 @@ require_once "connection/connection.php";
 require_once "responses.class.php";
 
 
-class shipping_address extends connection {
+class shipping_address extends connection
+{
 
-    public function getAddres($userId) {
+    private $userId = "";
+    private $name = "";
+    private $surname = "";
+    private $country = "";
+    private $address = "";
+    private $additional_address = "";
+    private $postal_code = "";
+    private $population = "";
+    private $province = "";
+    private $telephone_number = "";
+    private $email = "";
+
+    public function getAddres($userId)
+    {
 
         $query = "SELECT * FROM shipping_address WHERE userId = $userId";
         return parent::getData($query);
     }
 
+    public function post($json)
+    {
+
+        $_responses = new responses;
+        $data = json_decode($json, true);
+
+        if (!isset($data["name"]) || !isset($data["surname"]) || !isset($data["country"]) || !isset($data["address"]) || !isset($data["postal_code"]) || !isset($data["population"]) || !isset($data["province"]) || !isset($data["telephone_number"]) || !isset($data["email"])) {
+            return $_responses->error_400();
+        } else {
+
+
+            $this->name = $data["name"];
+            $this->surname = $data["surname"];
+            $this->country = $data["country"];
+            $this->address = $data["address"];
+            if (isset($data["additional_address"])) {
+                $this->additional_address = $data["additional_address"];
+            }
+            $this->postal_code = $data["postal_code"];
+            $this->population = $data["population"];
+            $this->province = $data["province"];
+            $this->telephone_number = $data["telephone_number"];
+            $this->email = $data["email"];
+
+            $resp = $this->insertAddress();
+
+            if ($resp) {
+                $response = $_responses->response;
+                $response["result"] = array(
+                    "id" => $resp
+                );
+                return $response;
+            } else {
+                return $_responses->error_500();
+            }
+        }
+    }
+
+    private function insertAddress()
+    {
+
+        $query = "INSERT INTO shipping_address (name, surname, country, address, additional_address, postal_code, population, province, telephone_number, email) VALUES(
+            '" . $this->name . "',
+            '" . $this->surname . "',
+            '" . $this->country . "',
+            '" . $this->address . "',
+            '" . $this->additional_address . "',
+            '" . $this->postal_code . "',
+            '" . $this->population . "',
+            '" . $this->province . "',
+            '" . $this->telephone_number . "',
+            '" . $this->email . "'
+        )";
+        $resp = parent::nonQueryId($query);
+
+        if ($resp) {
+            return $resp;
+        } else {
+            return 0;
+        }
+    }
+
+    public function put($json)
+    {
+
+        $_responses = new responses;
+        $data = json_decode($json, true);
+
+        if (!isset($data["userId"])) {
+            return $_responses->error_400();
+        } else {
+
+            $this->userId = $data["userId"];
+            if (isset($data["name"])) {
+                $this->name = $data["name"];
+            }
+            if (isset($data["surname"])) {
+                $this->surname = $data["surname"];
+            }
+            if (isset($data["country"])) {
+                $this->country = $data["country"];
+            }
+            if (isset($data["address"])) {
+                $this->address = $data["address"];
+            }
+            if (isset($data["additional_address"])) {
+                $this->additional_address = $data["additional_address"];
+            }
+            if (isset($data["postal_code"])) {
+                $this->postal_code = $data["postal_code"];
+            }
+            if (isset($data["population"])) {
+                $this->population = $data["population"];
+            }
+            if (isset($data["province"])) {
+                $this->province = $data["province"];
+            }
+            if (isset($data["telephone_number"])) {
+                $this->telephone_number = $data["telephone_number"];
+            }
+            if (isset($data["email"])) {
+                $this->email = $data["email"];
+            }
+
+            $resp = $this->updateAddress();
+            if ($resp) {
+                $response = $_responses->response;
+                $response["result"] = array(
+                    "id" => $this->userId
+                );
+                return $response;
+            } else {
+                return $_responses->error_500();
+            }
+        }
+    }
+
+    private function updateAddress()
+    {
+
+        $query = "UPDATE shipping_address SET 
+            name ='" . $this->name . "', 
+            surname = '" . $this->surname . "', 
+            country = '" . $this->country . "', 
+            address = '" . $this->address . "', 
+            additional_address = '" . $this->additional_address . "', 
+            postal_code = '" . $this->postal_code . "', 
+            population = '" . $this->population . "', 
+            province = '" . $this->province . "' ,
+            telephone_number = '" . $this->telephone_number . "' ,
+            email = '" . $this->email . "' 
+            WHERE id = " . $this->userId . "
+        ";
+
+        $resp = parent::nonQuery($query);
+        if ($resp >= 1) {
+            return $resp;
+        } else {
+            return 0;
+        }
+    }
+
+    public function delete($json)
+    {
+        $_responses = new responses;
+        $data = json_decode($json, true);
+
+        if (!isset($data["userId"])) {
+            return $_responses->error_400();
+        } else {
+
+            $this->userId = $data["userId"];
+            
+            $resp = $this->deleteAddress();
+            if ($resp) {
+                $response = $_responses->response;
+                $response["result"] = array(
+                    "id" => $this->userId
+                );
+                return $response;
+            } else {
+                return $_responses->error_500();
+            }
+        }
+    }
+
+    private function deleteAddress() {
+        $query = "DELETE FROM shipping_address WHERE userId = '" . $this->userId . "'";
+        $resp = parent::nonQuery($query);
+        if($resp >= 1) {
+            return $resp;
+        } else {
+            return 0;
+        }
+    }
 }
--- backend/shipping_address.php
+++ backend/shipping_address.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 require_once "class/responses.class.php";
 require_once "class/shipping_address.class.php";
@@ -6,24 +6,67 @@ require_once "class/shipping_address.class.php";
 $_responses = new responses;
 $_shipping_address = new shipping_address;
 
-if($_SERVER["REQUEST_METHOD"] == "GET") {
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
-    if(isset($_GET["userId"])) {
+    if (isset($_GET["userId"])) {
         $userId = $_GET["userId"];
         $address = $_shipping_address->getAddres($userId);
+        header("Content-Type: application/json");
         echo json_encode($address);
+        http_response_code(200);
     }
 
 } else if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    echo "POST";
+
+    // obtener los datos enviados
+    $postBody = file_get_contents("php://input");
+    // enviar los datos al controlador
+    $dataArray = $_shipping_address->post($postBody);
+    // devolvemos una respuesta
+    header("Content-Type: application/json");
+    if (isset($dataArray["result"]["error_id"])) {
+        $responseCode = $dataArray["result"]["error_id"];
+        http_response_code($responseCode);
+    } else {
+        http_response_code(200);
+    }
+    echo (json_encode($dataArray));
+
 } else if ($_SERVER["REQUEST_METHOD"] == "PUT") {
-    echo "PUT";
+
+    // obtener los datos a modificar
+    $postBody = file_get_contents("php://input");
+    // enviar los datos al controlador
+    $dataArray = $_shipping_address->put($postBody);
+    // devolvemos una respuesta
+    header("Content-Type: application/json");
+    if (isset($dataArray["result"]["error_id"])) {
+        $responseCode = $dataArray["result"]["error_id"];
+        http_response_code($responseCode);
+    } else {
+        http_response_code(200);
+    }
+    echo (json_encode($dataArray));
+
 } else if ($_SERVER["REQUEST_METHOD"] == "DELETE") {
-    echo "DELETE";
+    
+    // obtener los datos a modificar
+    $postBody = file_get_contents("php://input");
+    // enviar los datos al controlador
+    $dataArray = $_shipping_address->delete($postBody);
+    // devolvemos una respuesta
+    header("Content-Type: application/json");
+    if (isset($dataArray["result"]["error_id"])) {
+        $responseCode = $dataArray["result"]["error_id"];
+        http_response_code($responseCode);
+    } else {
+        http_response_code(200);
+    }
+    echo (json_encode($dataArray));
+
 } else {
 
     header("Content-Type: application/json");
     $dataArray = $_responses->error_405();
     echo json_encode($dataArray);
-
 }
