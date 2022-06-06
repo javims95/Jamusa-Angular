d861e6b88b05f9384ecb511033a0185c3660ab7f
javims95 <javier_ms95@hotmail.com>
2022-06-06 17:04:30 +0200

    Login validation with reactive forms

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 
@@ -9,24 +10,36 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class LoginComponent implements OnInit {
 
-  user = {
-    username: '',
-    password: ''
-  }
+  loginForm: FormGroup;
+  submitted = false;
 
   constructor(
     private userService: UserService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
   }
 
   login() {
-    this.userService.login(this.user).subscribe((res:any) => {
+    this.submitted = true;
+    
+    if(this.loginForm.invalid) {
+      return;
+    }
+
+    const user = this.loginForm.value;
+    this.userService.login(user).subscribe((res:any) => {
       localStorage.setItem('token', res.token);
       this.router.navigate(['my-account']);
     });
   }
+
+  get f() { return this.loginForm.controls; }
 
 }
